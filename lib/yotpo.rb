fa3e69b18494c468eeb5632ab6645402dc6c9e55
Vladislav Shub <vlad@yotpo.com>
2013-05-22 15:48:53 +0300

    moved oj encode and decode into the yotpo response parser

--- lib/yotpo.rb
+++ lib/yotpo.rb
@@ -3,23 +3,53 @@ require 'yotpo/client'
 
 module Yotpo
   class << self
-    attr_accessor :url, :parallel_requests, :app_key, :secret
+    # @!attribute url
+    # @return [String] the base url of the Yotpo Api
+    attr_accessor :url
 
+    # @!attribute parallel_requests
+    # @return [Integer String] defines the maximum parallel request for the gem to preform
+    attr_accessor :parallel_requests
+
+    # @!attribute app_key
+    # @return [String] the app key that is registered with Yotpo
+    attr_accessor :app_key
+
+    # @!attribute secret
+    # @return [String] the secret that is registered with Yotpo
+    attr_accessor :secret
+
+    # Configuration interface of the gem
+    #
+    # @yield [self] to accept configuration settings
     def configure
       yield self
       true
     end
 
+    #
+    # Makes sure that the method missing is checked with the Yotpo::Client instance
+    #
+    # @param method_name [String] the name of the method we want to run
+    # @param include_private [Boolean] defines wether to check for private functions as well
     def respond_to_missing?(method_name, include_private=false)
       client.respond_to?(method_name, include_private)
     end
 
+    #
+    # @return an instance of Yotpo::Client
+    #
     def client
       @client ||= Yotpo::Client.new()
     end
 
     private
 
+    #
+    # executes any function on the Yotpo::Client instance
+    #
+    # @param args [*] any argument that we want to pass to the client function
+    # @param block [Block] any block that is passed to the client function
     def method_missing(method_name, *args, &block)
       return super unless client.respond_to?(method_name)
       client.send(method_name, *args, &block)
--- lib/yotpo/client.rb
+++ lib/yotpo/client.rb
@@ -3,8 +3,6 @@ require 'faraday'
 require 'typhoeus'
 require 'typhoeus/adapters/faraday'
 require 'faraday_middleware'
-require 'yotpo/core/encode_oj'
-require 'yotpo/core/parse_oj'
 require 'yotpo/core/response_parser'
 require 'yotpo/version'
 require 'yotpo/api/account'
@@ -18,35 +16,67 @@ module Yotpo
   class Client
     include Yotpo::Review
 
+    #
+    # Creates a new instance of Yotpo::Client
+    #
+    # @param url [String] The url to yotpo api (https://api.yotpo.com)
+    # @param parallel_requests [Integer String] The maximum parallel request to do (5)
     def initialize(url = 'https://api.yotpo.com', parallel_requests = 5)
       @url = url
       @parallel_requests = parallel_requests
     end
 
+    #
+    # Does a GET request to the url with the params
+    #
+    # @param url [String] the relative path in the Yotpo API
+    # @param params [Hash] the url params that should be passed in the request
     def get(url, params = {})
       preform(url, :get, params: params) do
         return connection.get(url, params)
       end
     end
 
+    #
+    # Does a POST request to the url with the params
+    #
+    # @param url [String] the relative path in the Yotpo API
+    # @param params [Hash] the body of the request
     def post(url, params)
       preform(url, :post, params: params) do
         return connection.post(url, params)
       end
     end
 
+    #
+    # Does a PUT request to the url with the params
+    #
+    # @param url [String] the relative path in the Yotpo API
+    # @param params [Hash] the body of the request
     def put(url, params)
       preform(url, :put, params: params) do
         return connection.put(url, params)
       end
     end
 
+    #
+    # Does a DELETE request to the url with the params
+    #
+    # @param url [String] the relative path in the Yotpo API
     def delete(url)
       preform(url, :delete) do
         return connection.delete(url)
       end
     end
 
+    #
+    # Does a parallel request to the api for all of the requests in the block
+    #
+    # @example block
+    #   Yotpo.in_parallel do
+    #     Yotpo.create_review(review_params)
+    #     Yotpo.update_account(account_params)
+    #   end
     def in_parallel
       connection.in_parallel do
         yield
@@ -54,6 +84,13 @@ module Yotpo
     end
 
     private
+
+    #
+    # Preforms an HTTP request and notifies the ActiveSupport::Notifications
+    #
+    # @private
+    # @param url [String] the url to which preform the request
+    # @param type [String]
     def preform(url, type, params = {}, &block)
       ActiveSupport::Notifications.instrument 'Yotpo', request: type, url: url, params: params do
         if connection.in_parallel?
@@ -64,6 +101,8 @@ module Yotpo
       end
     end
 
+    #
+    # @return an instance of Faraday initialized with all that this gem needs
     def connection
       @connection ||= Faraday.new(url: @url, parallel_manager: Typhoeus::Hydra.new(max_concurrency: @parallel_requests), headers: {:yotpo_api_connector => Yotpo::VERSION}) do |conn|
 
