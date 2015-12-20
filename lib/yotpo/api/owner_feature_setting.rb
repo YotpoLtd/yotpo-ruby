fac4eb10286ebae5157276b9936eade99e521611
tovag <tgoldman@yotpo.com>
2015-12-20 11:18:03 +0200

    added owner_feature_settings_update

--- lib/yotpo/api/owner_feature_setting.rb
+++ lib/yotpo/api/owner_feature_setting.rb
@@ -8,11 +8,21 @@ module Yotpo
       get("features/#{params[:feature_id]}/get_multiple_owners_settings", request)
     end
 
-    def update_feature_settings(params)
+    def mass_update_feature_settings(params)
       request = {
-          utoken: params[:utoken]
+          utoken: params[:utoken],
+          settings: params[:settings]
       }
       put("features/#{params[:feature_id]}/#{params[:owner_id]}/admin_mass_update", request)
+    end
+
+    def update_feature_settings(params)
+      request = {
+          utoken: params[:utoken],
+          value: params[:value],
+          key: params[:key]
+      }
+      put("apps/#{params[:app_key]}/features/#{params[:feature_id]}/feature_settings/#{params[:feature_settings_id]}", request)
     end
   end
 end
