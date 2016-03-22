686f2be65a33ef491287f76ef67016db2b426f8a
Nimrod Popper <nimrod@yotpo.com>
2016-03-22 16:53:46 +0200

    add update account social

--- lib/yotpo/api/account_social.rb
+++ lib/yotpo/api/account_social.rb
@@ -3,22 +3,34 @@ module Yotpo
     def create_account_social(params)
       request = {
           account_social: {
-            social_network_name: params[:account_social][:social_network_name],
-            access_token: params[:account_social][:access_token],
-            social_id: params[:account_social][:social_id],
+            social_network_name: params[:social_network_name],
+            access_token: params[:access_token],
+            social_id: params[:social_id],
+            secret: params[:secret],
+            page_token: params[:page_token],
+            page_id: params[:page_id],
+            message: params[:message],
+            social_push_type_id: params[:social_push_type_id],
+            invalidated_at: params[:invalidated_at],
           },
           utoken: params[:utoken]
       }
-      app_key = params[:app_key]
-      post("/apps/#{app_key}/account_socials", request)
+      post("/apps/#{params[:app_key]}/account_socials", request)
+    end
+
+    def update_account_social(params)
+      request = {
+          utoken: params.delete(:utoken)
+      }
+      request[:account_social] = params
+      put("/apps/#{params.delete(:app_key)}/account_socials/#{params.delete(:id)}", request)
     end
 
     def get_account_social(params)
       request = {
           utoken: params[:utoken]
       }
-      app_key = params[:app_key]
-      get("/apps/#{app_key}/account_socials", request)
+      get("/apps/#{params[:app_key]}/account_socials", request)
     end
   end
 end
--- spec/api/account_social_spec.rb
+++ spec/api/account_social_spec.rb
@@ -6,11 +6,9 @@ describe Yotpo::AccountSocial do
       account_social_params = {
           utoken: @utoken,
           app_key: @app_key,
-          account_social: {
-            social_network_name: 'INSTAGRAM',
-            access_token: 78910,
-            social_id: 111213141516,
-          }
+          social_network_name: 'INSTAGRAM',
+          access_token: 78910,
+          social_id: 111213141516,
       }
       VCR.use_cassette('create_account_social') do
         @response = Yotpo.create_account_social(account_social_params)
@@ -21,6 +19,25 @@ describe Yotpo::AccountSocial do
     it { should be_a ::Hashie::Mash }
     it { should respond_to :access_token }
     it { should respond_to :social_id }
+  end
+
+  describe '#update social account' do
+    before(:all) do
+      account_social_params = {
+          utoken: @utoken,
+          app_key: @app_key,
+          access_token: 123,
+          secret: 123,
+          id: 17
+      }
+      VCR.use_cassette('update_account_social') do
+        @response = Yotpo.update_account_social(account_social_params)
+      end
+    end
+
+    subject { @response.body }
+    it { should be_a ::Hashie::Mash }
+    it { should respond_to :account_social }
   end
 
   describe '#get social account' do
