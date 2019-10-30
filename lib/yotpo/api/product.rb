1c4fcbebfed63222287ca1c7fc7b5a49dbde9c3f
Jim Butler <jim.butler@lifefuels.co>
2019-10-30 16:32:31 -0400

    Added new product route to Yotpo API

--- lib/yotpo/api/product.rb
+++ lib/yotpo/api/product.rb
@@ -63,5 +63,109 @@ module Yotpo
       app_key = params[:app_key]
       post("apps/#{app_key}/products_apps_matches/get_matches/", request, headers)
     end
+
+    # Creates a product(s) in Yotpo
+    #
+    # @param [Hash] params
+    # @option params [String] :app_key - Your Yotpo account access token
+    # @option params [String] :products - A hash of product(s) to create
+    # @option params [String] :return_email_address - Include an email address to receive a status report once your request is processed.
+    # @option params [String] :result_callback_url - Include a callback URL to receive an asynchronous update once your request is processed.
+    # See https://apidocs.yotpo.com/reference#create-mass-products for more info
+    def add_products(params, headers = {})
+      request = {
+          utoken: params[:utoken],
+          products: params[:products],
+          return_email_address: params[:return_email_address],
+          result_callback_url: params[:result_callback_url]
+      }
+
+      app_key = params[:app_key]
+      post("apps/#{app_key}/products/mass_create", request, headers)
+    end
+
+    # Updates a product(s) in Yotpo
+    #
+    # @param [Hash] params
+    # @option params [String] :app_key - Your Yotpo account access token
+    # @option params [String] :products - A hash of product(s) to update
+    # @option params [String] :return_email_address - Include an email address to receive a status report once your request is processed.
+    # @option params [String] :result_callback_url - Include a callback URL to receive an asynchronous update once your request is processed.
+    # See https://apidocs.yotpo.com/reference#update-mass-products for more info
+    def update_products(params, headers = {})
+      request = {
+        utoken: params[:utoken],
+        products: params[:products],
+        return_email_address: params[:return_email_address],
+        result_callback_url: params[:result_callback_url]
+      }
+
+      app_key = params[:app_key]
+      put("apps/#{app_key}/products/mass_update", request, headers)
+    end
+
+    # Gets all products
+    #
+    # @param [Hash] params
+    # @option params [String] :app_key - Your Yotpo account access token
+    # @option params [String] :count - Number of products to return (defaults to 10)
+    # @option params [String] :page - Page number to return products for (defaults to 1)
+    # See https://apidocs.yotpo.com/reference#retrieve-all-products for more info
+    def get_all_products(params, headers = {})
+      request = {
+          utoken: params[:utoken],
+          count: params[:count] || 10,
+          page: params[:page] || 1
+      }
+
+      app_key = params[:app_key]
+      get("apps/#{app_key}/products", request, headers)
+    end
+
+    # Add a product group in Yotpo
+    #
+    # @param [Hash] params
+    # @option params [String] :app_key - Your Yotpo account access token
+    # @option params [String] :group_name - The name of the product group
+    # See https://apidocs.yotpo.com/reference#create-a-product-group for more info
+    def add_product_group(params, headers = {})
+      request = {
+        utoken: params[:utoken],
+        group_name: params[:group_name]
+      }
+
+      app_key = params[:app_key]
+      post("apps/#{app_key}/products_groups", request, headers)
+    end
+
+    # Delete a product group in Yotpo
+    #
+    # @param [Hash] params
+    # @option params [String] :app_key - Your Yotpo account access token
+    # @option params [String] :group_name - The name of the product group
+    # See https://apidocs.yotpo.com/reference#delete-a-product-group for more info
+    def delete_product_group(params, headers = {})
+      request = {
+        utoken: params[:utoken],
+      }
+
+      app_key = params[:app_key]
+      group_name = params[:group_name]
+      delete("apps/#{app_key}/products_groups/#{group_name}", request, headers)
+    end
+
+    # Gets products group in Yotpo
+    #
+    # @param [Hash] params
+    # @option params [String] :app_key - Your Yotpo account access token
+    # See https://apidocs.yotpo.com/reference#retrieve-product-groups-for-account for more info
+    def get_product_groups(params, headers = {})
+      request = {
+        utoken: params[:utoken]
+      }
+
+      app_key = params[:app_key]
+      get("apps/#{app_key}/products_groups", request, headers)
+    end
   end
 end
